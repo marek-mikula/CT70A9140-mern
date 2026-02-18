d9e847e3fe94f0803312119831a8b7e960e771b3
Marek Mikula <marek.mikula01@gmail.com>
2026-02-18 14:55:57 +0200

    feat: changed the dashboard a little bit to be more garden-ish

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,29 +1,31 @@
-import {useEffect} from "react";
-import {useNavigate} from "react-router-dom";
-import {useAppDispatch, useAppSelector} from "../app/hooks.ts";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../app/hooks.ts";
 import FlowerForm from "../components/FlowerForm.tsx";
 import FlowerList from "../components/FlowerList.tsx";
 
 function Dashboard() {
     const navigate = useNavigate()
     const dispatch = useAppDispatch()
-
-    const {user} = useAppSelector(state => state.auth)
+    const { user } = useAppSelector(state => state.auth)
 
     useEffect(() => {
-        if (!user) {
-            navigate('/login')
-        }
+        if (!user) navigate('/login')
     }, [user, navigate, dispatch]);
 
     return (
-        <>
-            <header className="my-8">
-                <h1 className="text-xl tracking-tight font-semibold text-center">Welcome {user && user.name} to your garden!</h1>
+        <main className="pb-20">
+            <header className="py-12 text-center">
+                <h1 className="text-2xl md:text-3xl font-black text-emerald-950">
+                    Hello, <span className="text-emerald-500">{user?.name}</span>! 🌿
+                </h1>
             </header>
-            <FlowerForm/>
-            <FlowerList/>
-        </>
+
+            <div className="space-y-12">
+                <FlowerForm />
+                <FlowerList />
+            </div>
+        </main>
     )
 }
 
